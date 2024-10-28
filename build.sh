2aa43b60cd12620d1418a25848dce324917adebe
Ankur <107772408+Ankursupertramp@users.noreply.github.com>
2024-10-28 20:10:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# Install Python 3.11.7 using pyenv
+# Ensure pyenv is installed
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
+# Set up pyenv environment
 export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+
+# Install and use Python 3.11.7
 pyenv install -s 3.11.7
 pyenv global 3.11.7
 
 # Upgrade pip and install dependencies
 python -m pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt --target .
+pip install -r requirements.txt
+
+# If distutils is not included in the Python installation, install it manually
+if ! python -c "import distutils" &>/dev/null; then
+    pip install distutils
+fi
